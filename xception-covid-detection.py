09af15161811db7dd26234c0bf3b8a43068d040b
wade0125 <100984053+wade0125@users.noreply.github.com>
2022-04-27 15:40:24 +0800

    2022/4/27

--- xception-covid-detection.py
+++ xception-covid-detection.py
@@ -7,7 +7,7 @@ import tensorflow as tf
 gpus = tf.config.experimental.list_physical_devices('GPU')
 if gpus:
   try:
-    tf.config.experimental.set_virtual_device_configuration(gpus[0], [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=5120)])
+    tf.config.experimental.set_virtual_device_configuration(gpus[0], [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=10240)])
   except RuntimeError as e:
     print(e)
 from tensorflow.keras import layers, models
@@ -18,11 +18,12 @@ from sklearn.metrics import roc_auc_score, f1_score, classification_report, conf
 from plot_keras_history import show_history, plot_history
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
-
+import cv2
+import random
 
 #----------------------------------Load data------------------------------------------
 
-df_meta=pd.read_csv("C:/Users/GIGABYTE/Downloads/CoronaHack _Chest X-Ray_classify/Chest_xray_Corona_Metadata.csv",index_col=0)
+df_meta=pd.read_csv("C:/Users/user/Downloads/CoronaHack _Chest X-Ray_classify/Chest_xray_Corona_Metadata.csv",index_col=0)
 df_meta.head()
 
 train_datagen = ImageDataGenerator(preprocessing_function=tf.keras.applications.xception.preprocess_input,zoom_range=0.1,brightness_range=[0.5,1.3],
@@ -31,7 +32,7 @@ test_datagen=ImageDataGenerator(preprocessing_function=tf.keras.applications.xce
 
 
 BATCH_SIZE=64
-path="C:/Users/GIGABYTE/Downloads/CoronaHack _Chest X-Ray_classify/Coronahack-Chest-XRay-Dataset/Coronahack-Chest-XRay-Dataset/"
+path="C:/Users/user/Downloads/CoronaHack _Chest X-Ray_classify/Coronahack-Chest-XRay-Dataset/Coronahack-Chest-XRay-Dataset/"
 train_images=train_datagen.flow_from_dataframe(dataframe=df_meta[df_meta["Dataset_type"]=="TRAIN"],x_col='X_ray_image_name',y_col='Label',color_mode='rgb',class_mode='categorical',
                                                 target_size=(150,150),batch_size=BATCH_SIZE,shuffle=True,seed=1234,subset='training', directory=path+"/train")
 
@@ -49,11 +50,11 @@ output=layers.Dense(2, activation='sigmoid')(x)
 model=tf.keras.models.Model(inputs=inputs, outputs=output)
 model.compile(Adamax(learning_rate=1e-4), loss='binary_crossentropy',metrics=['accuracy'])
 history = model.fit(train_images, validation_data=val_images, epochs=30)
-model.save("C:/Users/GIGABYTE/Downloads/CoronaHack _Chest X-Ray_classify/Xception.h5")
+model.save("C:/Users/user/Downloads/CoronaHack _Chest X-Ray_classify/Xception.h5")
 
 show_history(history)
-plot_history(history, path="C:/Users/GIGABYTE/Downloads/CoronaHack _Chest X-Ray_classify/Training_history Xception.png",
-             title="Xception Training history")
+plot_history(history, path="C:/Users/user/Downloads/CoronaHack _Chest X-Ray_classify/Training_history Xception.png",
+              title="Xception Training history")
 plt.close()
 preds=model.predict(test_images)
 preds = np.argmax(preds,axis=1)
@@ -64,11 +65,32 @@ print(classification_report(gt,preds,target_names=["Normal","Covid"]))
 print("ROC AUC score:   ",roc_auc_score(gt,preds))
 print("F1 score:",f1_score(gt,preds))
 print("accuracy_score:",accuracy_score(gt,preds))
+model.save("C:/Users/user/Downloads/CoronaHack _Chest X-Ray_classify/Xception.h5")
 conf_matrix = confusion_matrix(gt, preds)
 sns.heatmap(conf_matrix,xticklabels = ["Normal","Covid"], yticklabels =["Normal","Covid"],annot=True,fmt='g')
 plt.title('Xception Confusion Matrix')
-plt.savefig("C:/Users/GIGABYTE/Downloads/CoronaHack _Chest X-Ray_classify/Confusion Matrix Xception.png")
+plt.savefig("C:/Users/user/Downloads/CoronaHack _Chest X-Ray_classify/Confusion Matrix Xception.png")
 plt.show()
+
+
+
+test_images.labels
+preds=model.predict(test_images)
+preds = np.argmax(preds,axis=1)
+preds=preds>0.5
+labels = (test_images.class_indices)
+labels = dict((v,k) for k,v in labels.items())
+predictions = [labels[k] for k in preds]
+real=[labels[k] for k in test_images.labels]
+filenames=test_images.filenames
+path_test="C:/Users/user/Downloads/CoronaHack _Chest X-Ray_classify/Coronahack-Chest-XRay-Dataset/Coronahack-Chest-XRay-Dataset/test/"
+r=random.randint(0,len(os.listdir(path_test)))
+img=cv2.imread(path_test+filenames[r])
+plt.imshow(img)
+plt.title("Xception results:Predicton:{0} | Real:{1}".format(predictions[r],real[r]))
+plt.savefig("C:/Users/user/Downloads/CoronaHack _Chest X-Ray_classify/Xception results EX.png")
+results=pd.DataFrame({"Filename":filenames,"real":real,"Predictions":predictions})
+results.to_csv("C:/Users/user/Downloads/CoronaHack _Chest X-Ray_classify/Xception results.csv",index=False)
 
 
 #----------------------------------VGG16------------------------------------------
@@ -80,10 +102,10 @@ output=layers.Dense(2, activation='sigmoid')(x)
 model=tf.keras.models.Model(inputs=inputs, outputs=output)
 model.compile(Adamax(learning_rate=1e-4), loss='binary_crossentropy',metrics=['accuracy'])
 history = model.fit(train_images, validation_data=val_images, epochs=30)
-model.save("C:/Users/GIGABYTE/Downloads/CoronaHack _Chest X-Ray_classify/VGG16.h5")
+model.save("C:/Users/user/Downloads/CoronaHack _Chest X-Ray_classify/VGG16.h5")
 
 show_history(history)
-plot_history(history, path="C:/Users/GIGABYTE/Downloads/CoronaHack _Chest X-Ray_classify/Training_history VGG16.png",
+plot_history(history, path="C:/Users/user/Downloads/CoronaHack _Chest X-Ray_classify/Training_history VGG16.png",
              title="Training history VGG16")
 plt.close()
 preds=model.predict(test_images)
@@ -98,8 +120,29 @@ print("accuracy_score:",accuracy_score(gt,preds))
 conf_matrix = confusion_matrix(gt, preds)
 sns.heatmap(conf_matrix,xticklabels = ["Normal","Covid"], yticklabels =["Normal","Covid"],annot=True,fmt='g')
 plt.title('VGG16 Confusion Matrix')
-plt.savefig("C:/Users/GIGABYTE/Downloads/CoronaHack _Chest X-Ray_classify/Confusion Matrix VGG16.png")
+plt.savefig("C:/Users/user/Downloads/CoronaHack _Chest X-Ray_classify/Confusion Matrix VGG16.png")
 plt.show()
+
+
+
+test_images.labels
+preds=model.predict(test_images)
+preds = np.argmax(preds,axis=1)
+preds=preds>0.5
+labels = (test_images.class_indices)
+labels = dict((v,k) for k,v in labels.items())
+predictions = [labels[k] for k in preds]
+real=[labels[k] for k in test_images.labels]
+filenames=test_images.filenames
+path_test="C:/Users/user/Downloads/CoronaHack _Chest X-Ray_classify/Coronahack-Chest-XRay-Dataset/Coronahack-Chest-XRay-Dataset/test/"
+r=random.randint(0,len(os.listdir(path_test)))
+img=cv2.imread(path_test+filenames[r])
+plt.imshow(img)
+plt.title("VGG16 results:Predicton:{0} | Real:{1}".format(predictions[r],real[r]))
+plt.savefig("C:/Users/user/Downloads/CoronaHack _Chest X-Ray_classify/VGG16 results EX.png")
+results=pd.DataFrame({"Filename":filenames,"real":real,"Predictions":predictions})
+results.to_csv("C:/Users/user/Downloads/CoronaHack _Chest X-Ray_classify/VGG16 results.csv",index=False)
+
 
 
 #----------------------------------VGG19------------------------------------------
@@ -111,11 +154,11 @@ output=layers.Dense(2, activation='sigmoid')(x)
 model=tf.keras.models.Model(inputs=inputs, outputs=output)
 model.compile(Adamax(learning_rate=1e-4), loss='binary_crossentropy',metrics=['accuracy'])
 history = model.fit(train_images, validation_data=val_images, epochs=30)
-model.save("C:/Users/GIGABYTE/Downloads/CoronaHack _Chest X-Ray_classify/VGG19.h5")
+model.save("C:/Users/user/Downloads/CoronaHack _Chest X-Ray_classify/VGG19.h5")
 
 
 show_history(history)
-plot_history(history, path="C:/Users/GIGABYTE/Downloads/CoronaHack _Chest X-Ray_classify/Training_history VGG19.png",
+plot_history(history, path="C:/Users/user/Downloads/CoronaHack _Chest X-Ray_classify/Training_history VGG19.png",
              title="Training history VGG19")
 plt.close()
 preds=model.predict(test_images)
@@ -130,8 +173,27 @@ print("accuracy_score:",accuracy_score(gt,preds))
 conf_matrix = confusion_matrix(gt, preds)
 sns.heatmap(conf_matrix,xticklabels = ["Normal","Covid"], yticklabels =["Normal","Covid"],annot=True,fmt='g')
 plt.title('VGG19 Confusion Matrix')
-plt.savefig("C:/Users/GIGABYTE/Downloads/CoronaHack _Chest X-Ray_classify/Confusion Matrix VGG19.png")
+plt.savefig("C:/Users/user/Downloads/CoronaHack _Chest X-Ray_classify/Confusion Matrix VGG19.png")
 plt.show()
+
+
+test_images.labels
+preds=model.predict(test_images)
+preds = np.argmax(preds,axis=1)
+preds=preds>0.5
+labels = (test_images.class_indices)
+labels = dict((v,k) for k,v in labels.items())
+predictions = [labels[k] for k in preds]
+real=[labels[k] for k in test_images.labels]
+filenames=test_images.filenames
+path_test="C:/Users/user/Downloads/CoronaHack _Chest X-Ray_classify/Coronahack-Chest-XRay-Dataset/Coronahack-Chest-XRay-Dataset/test/"
+r=random.randint(0,len(os.listdir(path_test)))
+img=cv2.imread(path_test+filenames[r])
+plt.imshow(img)
+plt.title("VGG19 results:Predicton:{0} | Real:{1}".format(predictions[r],real[r]))
+plt.savefig("C:/Users/user/Downloads/CoronaHack _Chest X-Ray_classify/VGG19 results EX.png")
+results=pd.DataFrame({"Filename":filenames,"real":real,"Predictions":predictions})
+results.to_csv("C:/Users/user/Downloads/CoronaHack _Chest X-Ray_classify/VGG19 results.csv",index=False)
 
 
 #----------------------------------InceptionResNet------------------------------------------
@@ -143,11 +205,11 @@ output=layers.Dense(2, activation='sigmoid')(x)
 model=tf.keras.models.Model(inputs=inputs, outputs=output)
 model.compile(Adamax(learning_rate=1e-4), loss='binary_crossentropy',metrics=['accuracy'])
 history = model.fit(train_images, validation_data=val_images, epochs=30)
-model.save("C:/Users/GIGABYTE/Downloads/CoronaHack _Chest X-Ray_classify/InceptionResNet.h5")
+model.save("C:/Users/user/Downloads/CoronaHack _Chest X-Ray_classify/InceptionResNet.h5")
 
 
 show_history(history)
-plot_history(history, path="C:/Users/GIGABYTE/Downloads/CoronaHack _Chest X-Ray_classify/Training_history InceptionResNet.png",
+plot_history(history, path="C:/Users/user/Downloads/CoronaHack _Chest X-Ray_classify/Training_history InceptionResNet.png",
              title="Training history InceptionResNet")
 plt.close()
 preds=model.predict(test_images)
@@ -162,12 +224,31 @@ print("accuracy_score:",accuracy_score(gt,preds))
 conf_matrix = confusion_matrix(gt, preds)
 sns.heatmap(conf_matrix,xticklabels = ["Normal","Covid"], yticklabels =["Normal","Covid"],annot=True,fmt='g')
 plt.title('InceptionResNet Confusion Matrix')
-plt.savefig("C:/Users/GIGABYTE/Downloads/CoronaHack _Chest X-Ray_classify/Confusion Matrix InceptionResNet.png")
+plt.savefig("C:/Users/user/Downloads/CoronaHack _Chest X-Ray_classify/Confusion Matrix InceptionResNet.png")
 plt.show()
 
+test_images.labels
+preds=model.predict(test_images)
+preds = np.argmax(preds,axis=1)
+preds=preds>0.5
+labels = (test_images.class_indices)
+labels = dict((v,k) for k,v in labels.items())
+predictions = [labels[k] for k in preds]
+real=[labels[k] for k in test_images.labels]
+filenames=test_images.filenames
+path_test="C:/Users/user/Downloads/CoronaHack _Chest X-Ray_classify/Coronahack-Chest-XRay-Dataset/Coronahack-Chest-XRay-Dataset/test/"
+r=random.randint(0,len(os.listdir(path_test)))
+img=cv2.imread(path_test+filenames[r])
+plt.imshow(img)
+plt.title("InceptionResNet results:Predicton:{0} | Real:{1}".format(predictions[r],real[r]))
+plt.savefig("C:/Users/user/Downloads/CoronaHack _Chest X-Ray_classify/InceptionResNet results EX.png")
+results=pd.DataFrame({"Filename":filenames,"real":real,"Predictions":predictions})
+results.to_csv("C:/Users/user/Downloads/CoronaHack _Chest X-Ray_classify/InceptionResNet results.csv",index=False)
+
+
 #----------------------------------EfficientNetB7------------------------------------------
-BATCH_SIZE=4
-path="C:/Users/GIGABYTE/Downloads/CoronaHack _Chest X-Ray_classify/Coronahack-Chest-XRay-Dataset/Coronahack-Chest-XRay-Dataset/"
+BATCH_SIZE=16
+path="C:/Users/user/Downloads/CoronaHack _Chest X-Ray_classify/Coronahack-Chest-XRay-Dataset/Coronahack-Chest-XRay-Dataset/"
 train_images=train_datagen.flow_from_dataframe(dataframe=df_meta[df_meta["Dataset_type"]=="TRAIN"],x_col='X_ray_image_name',y_col='Label',color_mode='rgb',class_mode='categorical',
                                                 target_size=(150,150),batch_size=BATCH_SIZE,shuffle=True,seed=1234,subset='training', directory=path+"/train")
 
@@ -186,11 +267,11 @@ output=layers.Dense(2, activation='sigmoid')(x)
 model=tf.keras.models.Model(inputs=inputs, outputs=output)
 model.compile(Adamax(learning_rate=1e-4), loss='binary_crossentropy',metrics=['accuracy'])
 history = model.fit(train_images, validation_data=val_images, epochs=30)
-model.save("C:/Users/GIGABYTE/Downloads/CoronaHack _Chest X-Ray_classify/EfficientNetB7.h5")
+model.save("C:/Users/user/Downloads/CoronaHack _Chest X-Ray_classify/EfficientNetB7.h5")
 
 
 show_history(history)
-plot_history(history, path="C:/Users/GIGABYTE/Downloads/CoronaHack _Chest X-Ray_classify/Training_history EfficientNetB7.png",
+plot_history(history, path="C:/Users/user/Downloads/CoronaHack _Chest X-Ray_classify/Training_history EfficientNetB7.png",
              title="Training history EfficientNetB7")
 plt.close()
 preds=model.predict(test_images)
@@ -205,8 +286,32 @@ print("accuracy_score:",accuracy_score(gt,preds))
 conf_matrix = confusion_matrix(gt, preds)
 sns.heatmap(conf_matrix,xticklabels = ["Normal","Covid"], yticklabels =["Normal","Covid"],annot=True,fmt='g')
 plt.title('EfficientNetB7 Confusion Matrix')
-plt.savefig("C:/Users/GIGABYTE/Downloads/CoronaHack _Chest X-Ray_classify/Confusion Matrix EfficientNetB7.png")
+plt.savefig("C:/Users/user/Downloads/CoronaHack _Chest X-Ray_classify/Confusion Matrix EfficientNetB7.png")
 plt.show()
+
+
+
+test_images.labels
+preds=model.predict(test_images)
+preds = np.argmax(preds,axis=1)
+preds=preds>0.5
+labels = (test_images.class_indices)
+labels = dict((v,k) for k,v in labels.items())
+predictions = [labels[k] for k in preds]
+real=[labels[k] for k in test_images.labels]
+filenames=test_images.filenames
+path_test="C:/Users/user/Downloads/CoronaHack _Chest X-Ray_classify/Coronahack-Chest-XRay-Dataset/Coronahack-Chest-XRay-Dataset/test/"
+r=random.randint(0,len(os.listdir(path_test)))
+img=cv2.imread(path_test+filenames[r])
+plt.imshow(img)
+plt.title("EfficientNetB7 results:Predicton:{0} | Real:{1}".format(predictions[r],real[r]))
+plt.savefig("C:/Users/user/Downloads/CoronaHack _Chest X-Ray_classify/EfficientNetB7 results EX.png")
+results=pd.DataFrame({"Filename":filenames,"real":real,"Predictions":predictions})
+results.to_csv("C:/Users/user/Downloads/CoronaHack _Chest X-Ray_classify/EfficientNetB7 results.csv",index=False)
+
+
+
+
 
 
 
